ae80adad89b40f5d6cafe705e5bf01653c686785
IlyaLevchenko04 <nneo2086@gmail.com>
2023-03-18 16:11:53 +0200

    re

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -48,7 +48,15 @@ export const App = () => {
         return alert('There are no pictures');
       }
 
-      setApiRespond(state => [...state, ...hits]);
+      const mapedHits = hits.map(({ id, webformatURL, largeImageURL }) => {
+        return {
+          id,
+          webformatURL,
+          largeImageURL,
+        };
+      });
+
+      setApiRespond(state => [...state, ...mapedHits]);
       setTotalImages(totalHits);
       setStatus('resolved');
     });
@@ -83,100 +91,3 @@ export const App = () => {
     );
   }
 };
-
-// export class App extends Component {
-//   state = {
-//     q: '',
-//     apiRespond: [],
-//     modalImage: '',
-//     status: 'idle',
-//     modal: false,
-//     pageCounter: 1,
-//     totalImages: 0,
-//   };
-
-// componentDidUpdate(prevProps, prevState) {
-//   const { q, pageCounter } = this.state;
-//   if (q !== prevState.q || pageCounter !== prevState.pageCounter) {
-//     this.setState({ status: 'pending' });
-
-//     getImages(q, pageCounter).then(({ hits, totalHits }) => {
-//       if (hits.length === 0) {
-//         this.setState({ status: 'idle' });
-//         return alert('There are no pictures');
-//       }
-
-//       return this.setState(prevState => {
-//         return {
-//           apiRespond: [...prevState.apiRespond, ...hits],
-//           totalImages: totalHits,
-//           status: 'resolved',
-//         };
-//       });
-//     });
-//   }
-// }
-
-// onSubmit = q => {
-//   this.setState({ q, pageCounter: 1, apiRespond: [], totalImages: 0 });
-// };
-
-// handleModal = () => {
-//   return this.setState(prevState => {
-//     return { modal: !prevState.modal };
-//   });
-// };
-
-// handleLargeImage = (modalImage = '') => {
-//   this.handleModal();
-//   this.setState({ modalImage });
-// };
-
-// handleLoadMore = () => {
-//   this.setState(prevState => ({
-//     pageCounter: prevState.pageCounter + 1,
-//   }));
-// };
-
-//   render() {
-//     const { status, modal, apiRespond, modalImage, totalImages } = this.state;
-
-// if (status === 'idle') {
-//   return <Searchbar onSubmit={this.onSubmit} />;
-// }
-
-// if (status === 'pending') {
-//   return (
-//     <>
-//       <Searchbar onSubmit={this.onSubmit} />
-//       <ImageGallery
-//         handleLargeImage={this.handleLargeImage}
-//         resp={apiRespond}
-//       />
-//       <Loader />
-//     </>
-//   );
-// }
-
-// if (status === 'resolved') {
-//   return (
-//     <>
-//       <Searchbar onSubmit={this.onSubmit} />
-//       <ImageGallery
-//         handleLargeImage={this.handleLargeImage}
-//         resp={apiRespond}
-//       />
-//       {apiRespond.length !== totalImages && (
-//         <Button handleLoadMore={this.handleLoadMore} />
-//       )}
-//       {modal && (
-//         <Modal
-//           handleLargeImage={this.handleLargeImage}
-//           imageInfo={modalImage}
-//         />
-//       )}
-//     </>
-//   );
-// }
-//   }
-// }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,12 +2,6 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 export const Modal = ({ imageInfo, handleLargeImage }) => {
-  const onEscClick = e => {
-    if (e.code === 'Escape') {
-      return handleLargeImage();
-    }
-  };
-
   const onBakcdropClick = e => {
     if (e.currentTarget === e.target) {
       return handleLargeImage();
@@ -15,14 +9,17 @@ export const Modal = ({ imageInfo, handleLargeImage }) => {
   };
 
   useEffect(() => {
+    const onEscClick = e => {
+      if (e.code === 'Escape') {
+        return handleLargeImage();
+      }
+    };
     window.addEventListener('keydown', onEscClick);
-  });
 
-  useEffect(() => {
     return () => {
       window.removeEventListener('keydown', onEscClick);
     };
-  });
+  }, [handleLargeImage]);
 
   return (
     <div className="overlay" onClick={onBakcdropClick}>
@@ -32,38 +29,6 @@ export const Modal = ({ imageInfo, handleLargeImage }) => {
     </div>
   );
 };
-
-// export class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.onEscClick);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.onEscClick);
-//   }
-
-// onEscClick = e => {
-//   if (e.code === 'Escape') {
-//     return this.props.handleLargeImage();
-//   }
-// };
-
-// onBakcdropClick = e => {
-//   if (e.currentTarget === e.target) {
-//     return this.props.handleLargeImage();
-//   }
-// };
-
-//   render() {
-//     return (
-//       <div className="overlay" onClick={this.onBakcdropClick}>
-//         <div className="modal">
-//           <img src={this.props.imageInfo} alt="" />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 Modal.propTypes = {
   handleLargeImage: PropTypes.func.isRequired,
